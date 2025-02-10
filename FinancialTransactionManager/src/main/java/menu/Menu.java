731dfa80a403796f07febaef0a3c5fb040f1dfb9
JB1302 <jstevenbarrantes@gmail.com>
2025-02-09 23:03:45 -0600

    Indentation y Logica

--- FinancialTransactionManager/src/main/java/menu/Menu.java
+++ FinancialTransactionManager/src/main/java/menu/Menu.java
@@ -7,11 +7,13 @@ import java.util.HashMap;
 
 public class Menu {
 
+    // Crear HashMap para base de Datos
     public static HashMap<Integer, Transaccion.RegistroTransaccion> baseDeDatos = new HashMap<>();
 
     public static void main(String[] args) {
-        Transaccion transaccion = new Transaccion() {
-        };
+        // Crear objeto Transtaccioón
+        Transaccion transaccion = new Transaccion() {};
+        // Crear objeto Ingreso
         Ingreso ingreso = new Ingreso();
 
         // Procesamos la transacción
@@ -19,12 +21,13 @@ public class Menu {
 
         // Imprimir en consola
         System.out.println("\n");
-        System.out.println("\n");
 
+        // Mostrar Transacciones
         transaccion.mostrarTransacciones();
 
+        //Calcular Impuestos con Transacciones
         transaccion.calcularImpuestos(150);
-
+        //Calcular Impuestos de Ingreso
         ingreso.calcularImpuestos(150);
 
 
