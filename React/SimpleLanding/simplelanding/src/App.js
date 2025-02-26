3c022f8f9a5652df51cf49f747308c07587ad9a2
JB1302 <jstevenbarrantes@gmail.com>
2025-02-25 23:39:17 -0600

    Finalized iTEM

--- React/SimpleLanding/simplelanding/src/App.js
+++ React/SimpleLanding/simplelanding/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
 import Home from './components/Home';
-import About from './components/About'
+import About from './components/About';
 import Work from './components/Work'
 import Testimonials from './components/Testimonials';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
@@ -12,6 +15,8 @@ function App() {
            <About />
            <Work />
             <Testimonials />
+            <Contact />
+            <Footer />
         </div>
     </div>
   );
