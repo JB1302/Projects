d6bedc25a6e406f15bfea9a01d39f8b0c84d3b03
JB1302 <jstevenbarrantes@gmail.com>
2025-03-20 00:34:37 -0600

    main enable

--- Java/SGV/src/main/java/Main.java
+++ Java/SGV/src/main/java/Main.java
@@ -5,17 +5,14 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-/*
+
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
                 new Login().setVisible(true);
             }
-        });*/
+        });
 
-        ReparacionController reparacionController = new ReparacionController();
-
-        reparacionController.insertarObjeto();
 
     }
 }
